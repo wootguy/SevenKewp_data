3cc3b487ec44332e0ded7b37f6bf62cfd92a2ec6
wootguy <drake.rhunter@gmail.com>
2024-12-11 13:32:49 -0800

    update fog generator script and remove offset from fog
    buy using a combination of additive and texture render modes, colored fog can be made full bright without the EF_BRIGHTLIGHT effect. This might also be used to make full-bright textures later, but requires 2 rendering passes per model and adds a good deal of complexity.

--- models/hlcoop_v2/weather/src/fog_dither.py
+++ models/hlcoop_v2/weather/src/fog_dither.py
@@ -1,3 +1,7 @@
+# This script has code for generating a fog model using solid pixel dither patterns in alpha-tested textures.
+# It does the job of fog but is extremely distracting and chaotic looking.
+# This method doesn't break transparent ents and requires fewer models than the additive method.
+
 import math, colorsys, os, random, numpy
 from PIL import Image
 
@@ -56,13 +60,12 @@ def gen_smd(file_path, file_out, u, v):
 						x, y, z = map(float, parts[1:4])  # Extract vertex coordinates
 						vertices.add((x, y, z))
 						
-						#uScale = 12 / 11
-						#tiling = 4.0
-						#u,v = generate_sphere_coordinates(x, y, z)
-						#u *= tiling * uScale
-						#v *= tiling
-						#outfile.write("%s %f %f %f %f %f %f %f %f\n" % (parts[0], x, y, z, -x*0.1, -y*0.1, -z*0.1, u, v))
-						outfile.write("%s %f %f %f %f %f %f %f %f\n" % (parts[0], x, y, z, -x, -y, -z, u, v))
+						uScale = 12 / 11
+						tiling = 4.0
+						u,v = generate_sphere_coordinates(x, y, z)
+						u *= tiling * uScale
+						v *= tiling
+						outfile.write("%s %f %f %f 0.0 0.0 1.0 %f %f\n" % (parts[0], x, y, z, u, v))
 					except ValueError as e:
 						print (e)
 						continue  # Skip lines that don't represent vertices
@@ -94,6 +97,7 @@ dither_patterns = [
 def gen_textures():
 	global pad
 	global hue_count
+	global dither_patterns
 	
 	hues = generate_hue_wheel_palette(hue_count)
 	hues = [0] + hues
@@ -101,48 +105,77 @@ def gen_textures():
 	palidx = 0
 	
 	with open('colors.c', 'w') as file:
-		file.write("int g_fog_skins = %d;\n\n" % (hue_count+1))
+		file.write("const int g_fog_skins = %d;\n\n" % (hue_count+1))
 		file.write("unsigned int g_fog_palette[%d][256] = {\n" % (hue_count+1))
 		
-		for hueidx, hue in enumerate(hues):
-
-			img = Image.new('RGB', (16*pad, 16*pad))
-			pixels = img.load()
-		
-			file.write("\t{")
-			for y in range(0, 16, 1):
-				for x in range(0, 16, 1):
-					sat = (y / 15.0) * 0.9 + 0.1
-					bright = (x / 15.0) * 0.9 + 0.1
+		for dither_level in range(len(dither_patterns) + 1):
+			for hueidx, hue in enumerate(hues):
+				'''
+				#img = Image.new('RGB', (16*pad, 16*pad))
+				img = Image.new('RGB', (16, 16))
+				pixels = img.load()
+			
+				file.write("\t{")
+				#for y in range(0, 16, 1):
+				#	for x in range(0, 16, 1):
+				#sat = (y / 15.0) * 0.9 + 0.1
+				#bright = (x / 15.0) * 0.9 + 0.1
+				
+				#if hueidx == 0:
+				#	sat = 0 # first pal is greyscale
+				#	bright = (y*16 + x) / 255
+				
+				#r, g, b = colorsys.hsv_to_rgb(hue, sat, bright)
+				r, g, b = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+				r = int(r * 255)
+				g = int(g * 255)
+				b = int(b * 255)
+				
+				file.write(f"0x{r:02X}{g:02X}{b:02X}")
+				#if y!= 15 or x != 15:
+				#	file.write(',')
+				#	file.write(',')
+				
+				for px in range(0, 16):
+					for py in range(0, 16):
+						if dither_level == 0 or dither_patterns[dither_level-1][px % 4][py % 4]:
+							#pixels[x*pad + px, y*pad + py] = (r, g, b)
+							pixels[px, py] = (64, 0, 0)
+						else:
+							pixels[px, py] = (0,0,0)
+				file.write("},\n")
+				'''
+				img = Image.open("clouds_0.bmp")
+				img = img.quantize()
+				print('{ "pal_%d.bmp" }' % palidx)
+				
+				palette = img.getpalette()
+				pal_pixels = img.getdata()
+				new_pixels = []
+				new_pal = palette[:]
+				
+				for c in pal_pixels:
+					r = palette[c*3 + 0]
+					g = palette[c*3 + 1]
+					b = palette[c*3 + 2]
 					
-					if hueidx == 0:
-						sat = 0 # first pal is greyscale
-						bright = (y*16 + x) / 255
+					if r == 0 and g == 0 and b == 0:
+						new_pixels.append(255) # map blak to transparent
+					else:
+						new_pixels.append(c)
 					
-					r, g, b = colorsys.hsv_to_rgb(hue, sat, bright)
-					r = int(r * 255)
-					g = int(g * 255)
-					b = int(b * 255)
-					
-					file.write(f"0x{r:02X}{g:02X}{b:02X}")
-					if y!= 15 or x != 15:
-						file.write(',')
-					
-					for px in range(0, pad):
-						for py in range(0, pad):
-							pixels[x*pad + px, y*pad + py] = (r, g, b)
-			file.write("},\n")
-
-			img = img.quantize()
-			print('{ "pal_%d.bmp" }' % palidx)
-
-			img.save("pal_%d.bmp" % palidx)
-			palidx += 1
+				new_pal[255*3] = 0
+				new_pal[255*3+1] = 0
+				new_pal[255*3+2] = 255
+				
+				img.putpalette(new_pal)
+				img.putdata(new_pixels)
+				img.save("pal_%d.bmp" % palidx)
+				palidx += 1
 			
 		file.write("};\n")
 
 def gen_bodies():
-	global offset_z
 	global pad
 	
 	sz = 16*pad
@@ -162,7 +195,6 @@ def gen_qc(num_bodies, num_pal):
 		file.write('$cd "."\n')
 		file.write('$cdtexture "."\n')
 		file.write('$scale 1.0\n\n')
-		file.write('$origin 0 0 %d\n\n' % offset_z)
 		
 		file.write('$bodygroup body\n{\n')
 		for i in range(0, num_bodies):
@@ -174,8 +206,8 @@ def gen_qc(num_bodies, num_pal):
 			file.write('{ "pal_%d.bmp" }\n' % i)
 		file.write('}\n\n')
 		
-		#for i in range(0, num_pal):
-			#file.write('$texrendermode "pal_%d.bmp" additive\n' % i)
+		for i in range(0, num_pal):
+			file.write('$texrendermode "pal_%d.bmp" masked\n' % i)
 		
 		file.write('$sequence idle "idle" fps 1 ACT_IDLE 1\n')
 		for d in sphere_dists:
@@ -218,10 +250,7 @@ def gen_bones(file_path, file_out):
 		outfile.write("version 1\n")
 		outfile.write("nodes\n")
 		for idx, bone in enumerate(bones):
-			if idx == 0:
-				outfile.write('0 "root" -1\n')
-			else:
-				outfile.write('%d "v%d" 0\n' % (idx, idx))
+			outfile.write('%d "v%d" %s\n' % (idx, idx, 0 if idx else -1))
 		outfile.write("end\n")
 		
 		outfile.write("skeleton\n")
@@ -249,7 +278,7 @@ def gen_bones(file_path, file_out):
 						
 						for idx, bone in enumerate(bones):
 							if abs(x - bone[0]) < 0.01 and abs(y - bone[1]) < 0.01 and abs(z - bone[2]) < 0.01:
-								outfile.write("%d %f %f %f %f %f %f 0.0 0.0\n" % (idx, x, y, z, -x, -y, -z))
+								outfile.write("%d %s\n" % (idx, " ".join(no_bones_line)))
 								break
 					except ValueError as e:
 						print (e)
@@ -265,10 +294,7 @@ def gen_anim(file_out, bones, scale, offset):
 		outfile.write("version 1\n")
 		outfile.write("nodes\n")
 		for idx, bone in enumerate(bones):
-			if idx == 0:
-				outfile.write('0 "root" -1\n')
-			else:
-				outfile.write('%d "v%d" 0\n' % (idx, idx))
+			outfile.write('%d "v%d" %s\n' % (idx, idx, 0 if idx else -1))
 		outfile.write("end\n")
 		
 		outfile.write("skeleton\n")
@@ -283,27 +309,24 @@ def gen_anim(file_out, bones, scale, offset):
 			outfile.write('%d %f %f %f 0.0 0.0 0.0\n' % (idx, bone[0]*scale, bone[1]*scale, bone[2]*scale + f))
 		outfile.write("end\n")
 
-pad = 3 # size of a color pixel in each palette texture, to prevent color leaking (happens even with tex filtering off)
+pad = 4 # size of a color pixel in each palette texture, to prevent color leaking (happens even with tex filtering off)
 
 # up to 255 is possible (save 1 for greyscale), but there are diminishing returns past 32 or so and higher costs:
 # longer precache time, file size, possibly client crashes sooner due to texture load leak(?)
 # number should divide 360 evenly so that you can get pure green/blue/etc. (green is 120 degrees from red)
-hue_count = 60
-#hue_count = 0
+#hue_count = 60
+hue_count = 0
 
 # skeleton will be scaled by these amounts, with an animation for each scale. Keyframe 0 = scale 1, keyframe 1 = scale
 sphere_dists = [256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072]
 
-# z offset applied so that EF_BRIGHTLIGHT effect is hidden
-offset_z = 0
-
 bones = gen_bones('fog.smd', 'fog_bones.smd')
 gen_anim('idle.smd', bones, 1.0, 0)
 for d in sphere_dists:
-	gen_anim('dist_%d.smd' % d, bones, d, 0)
+	gen_anim('dist_%d.smd' % d, bones, d, 16384)
 
 gen_textures()
 gen_bodies()
-gen_qc(256, hue_count+1)
+gen_qc(256, hue_count+1 * 8)
 os.system("studiomdl fog.qc")
 os.system("xcopy fog.mdl C:\\Games\\Steam\\steamapps\\common\\Half-Life\\valve_downloads\\models\\hlcoop_v2\\weather\\fog.mdl /Y")
